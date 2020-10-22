70a1118825a1e21da9bf376fba2451167c6b25d0
Jesse Kelly <kellyjesse881@gmail.com>
2020-10-22 11:50:11 -0500

    feat: added reducer prop to H

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import * as h from "react-hyperscript";
 import * as tagNames from "html-tag-names";
 import * as ReactDom from "react-dom";
-import { _capture, reducerComponent, make, update } from "react-fp-ts";
+import {
+    _capture,
+    reducerComponent,
+    make,
+    update,
+    Self,
+    StateUpdate,
+} from "react-fp-ts";
 
 type OmitFirstArg<F> = F extends (x: any, ...args: infer P) => infer R
     ? (...args: P) => R
     : never;
 
-const tags = tagNames
+export const tags = tagNames
     .map((tag: string) => ({
         [tag]: (...args: Parameters<OmitFirstArg<typeof h>>) => h(tag, ...args),
     }))
@@ -25,27 +32,36 @@ export const render = (
     selector: string = "root",
 ) => ReactDom.render(e, document.querySelector(selector));
 
-const hMap = (el: typeof h) => (arr: Parameters<typeof h>[]) =>
+export const hMap = (el: typeof h) => (arr: Parameters<typeof h>[]) =>
     arr.map((o, key) => el({ key }, o));
 
-const H = (name = "", stateObj = {}) => ({
-    // @param render :: state => H
-    of: render =>
-        H(name, {
-            ...stateObj,
-            render,
-        }),
+export function H<State, Action>(name = "", stateObj = {}) {
+    type HSelf = Self<{}, State, Action>;
 
-    // @param initialValue :: initialValue => H
-    initialState: initialState =>
-        H(name, {
-            ...stateObj,
-            initialState,
-        }),
+    return {
+        // @param render :: state => H
+        of: (render: (x: HSelf) => typeof h) =>
+            H(name, { ...stateObj, render }),
 
-    render: () => (...args) =>
-        h(make(reducerComponent(name), stateObj), ...args),
-});
+        // @param initialValue :: initialValue => H
+        initialState: (initialState: State) =>
+            H(name, {
+                ...stateObj,
+                initialState,
+            }),
+
+        reducer: (r: (s: HSelf, a: Action) => StateUpdate) => {
+            const reducer = (s: HSelf, a: Action) => {
+                const ret = r(s, a);
+                return update(ret);
+            };
+
+            return H(name, { ...stateObj, reducer });
+        },
+
+        create: () => () => h(make(reducerComponent(name), stateObj)),
+    };
+}
 
 module.exports = {
     h, // react-hyperscript h
